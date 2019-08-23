9dc4468955a8abc6b74001d82e47179e2a7aa04d
Mike Cooper <mikescooper@icloud.com>
2019-08-23 04:13:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  min_val = hash.first 
-  min_key = 
+  min_val = hash.first[1]
+  min_key = hash.first[0]
   
   hash.each do 
     
